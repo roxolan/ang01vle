1f85144b259e467647ee0d5b106fea4982acdffb
kmbs <victor.kotusenko@kmbs.ua>
2015-03-02 11:37:52 +0200

    add javascript angular-related libraries, autocrlf disabled

---
+++ ang01vle/App/app.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    // create the app
+    var app = angular.module('app', [
+      // ootb angular modules
+      'ngRoute',      // app route (url path) support
+      'ngSanitize'  // fixes HTML issues in data binding
+      //'ngCookies',   // cookie read/write support
+      // my custom modules
+      //'common'
+    ]);
+
+    // wire up routing for errors & success events
+    //app.run(['$route', function ($route) {
+        // run any code that needs to execute prior to the app actually starting up
+    //}]);
+})();
