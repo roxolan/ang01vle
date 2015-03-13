d78697b5fdbf15877b0bb70c1ad61edf0ee71b32
kmbs <victor.kotusenko@kmbs.ua>
2015-03-13 11:52:10 +0200

    added initial models as stubs

---
+++ ang01vle/App/models/course.js
@@ -0,0 +1,15 @@
+﻿// create namespace for this project
+var vle = vle || {};
+vle.models = vle.models || {};
+
+// learning item entity
+vle.models.course = function () {
+    this.Id = undefined;
+    this.Title = undefined;
+    this.LearningGroupId = undefined;
+    this.DetailLink = undefined;
+    this.OData__Comments = undefined;
+    this.__metadata = {
+        type: 'SP.Data.CourseListItem'
+    };
+};
---
+++ ang01vle/App/models/learningGroup.js
@@ -0,0 +1,14 @@
+﻿// create namespace for this project
+var vle = vle || {};
+vle.models = vle.models || {};
+
+// learning path entity
+vle.models.learningGroup = function () {
+    this.Id = undefined;
+    this.Title = undefined;
+    this.OData__Comments = undefined;
+    this.__metadata = {
+        type: 'SP.Data.LearningGroupListItem'
+    };
+};
+
