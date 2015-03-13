cf61720b9fa7194c6c5a057657c60138d62d3aa7
kmbs <victor.kotusenko@kmbs.ua>
2015-03-13 11:55:06 +0200

    add angular config to add json header in any http request

---
+++ ang01vle/App/services/datacontext.angular.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    var serviceId = 'datacontext';
+    angular.module('app').factory(serviceId,
+        ['$rootScope', '$http', '$q', 'config', 'common', 'spContext', datacontext]);
+
+    function datacontext($rootScope, $http, $q, config, common, spContext) {
+
+        init()
+
+        // service public signature
+        return {}
+
+        function init() {
+            common.logger.log("service loaded", null, serviceId);
+        }
+    }
+
+})();
