b59e82089d77ed590e42762323ae4b8565cd3f83
kmbs <victor.kotusenko@kmbs.ua>
2015-03-21 11:17:19 +0200

    implement deleting lgroup

--- ang01vle/App/learningGroup/learningGroupDetail.js
+++ ang01vle/App/learningGroup/learningGroupDetail.js
@@ -1,72 +1,95 @@
 ﻿(function () {
-    'use strict';
+  'use strict';
 
-    // define controller
-    var controllerId = "learningGroupDetail";
-    angular.module('app').controller(controllerId,
-      ['$window', '$location', '$routeParams', 'common', 'datacontext', learningGroupDetail]);
+  // define controller
+  var controllerId = "learningGroupDetail";
+  angular.module('app').controller(controllerId,
+    ['$window', '$location', '$routeParams', 'common', 'datacontext', learningGroupDetail]);
 
-    // create controller
-    function learningGroupDetail($window, $location, $routeParams, common, datacontext) {
-        var vm = this;
-        // utility method to convert universal time > local time using moment.js
-        vm.created = localizedCreatedTimestamp;
-        vm.modified = localizedModifiedTimestap;
-        // navigate backwards in the history stack
-        vm.goBack = goBack;
+  // create controller
+  function learningGroupDetail($window, $location, $routeParams, common, datacontext) {
+    var vm = this;
+    // utility method to convert universal time > local time using moment.js
+    vm.created = localizedCreatedTimestamp;
+    vm.modified = localizedModifiedTimestap;
+    // navigate backwards in the history stack
+    vm.goBack = goBack;
+    vm.goSave = goSave;
+    vm.goDelete = goDelete;
 
-        // init controller
-        init();
+    // init controller
+    init();
 
 
-        // init controller
-        function init() {
-            // if an ID is passed in, load the item
-            var learningGroupId = +$routeParams.id;
-            if (learningGroupId && learningGroupId > 0) {
+    // init controller
+    function init() {
+      // if an ID is passed in, load the item
+      var learningGroupId = +$routeParams.id;
+      if (learningGroupId && learningGroupId > 0) {
 
-                getItemDetail(learningGroupId);
-                // some future version:
-                // getItem(learningPathId);
-            } else {
-                createItem();
-            }
-
-            common.logger.log("controller loaded", null, controllerId);
-            common.activateController([], controllerId);
+        getItemDetail(learningGroupId);
+          // some future version:
+          // getItem(learningPathId);
+        } else {
+          createItem();
         }
 
-        // localized created time for the current item
-        function localizedCreatedTimestamp() {
-            if (vm.learningGroup) {
-                return moment(vm.learningGroup.Created).format("M/D/YYYY h:mm A");
-            } else {
-                return '';
-            }
-        }
+      common.logger.log("controller loaded", null, controllerId);
+      common.activateController([], controllerId);
+    }
+
+    // localized created time for the current item
+    function localizedCreatedTimestamp() {
+      if (vm.learningGroup) {
+        return moment(vm.learningGroup.Created).format("M/D/YYYY h:mm A");
+      } else {
+        return '';
+      }
+    }
 
         // localized modified time for the current item
-        function localizedModifiedTimestap() {
-            if (vm.learningGroup) {
-                return moment(vm.learningGroup.Modified).format("M/D/YYYY h:mm A");
-            } else {
-                return '';
-            }
-        }
-
-        // navigate backwards in the history stack
-        function goBack() {
-            $window.history.back();
-        }
-
-        // load the item specified in the route
-        function getItemDetail(learningGroupId) {
-            datacontext.getLearningGroup(learningGroupId)
-              .then(function (data) {
-                vm.learningGroup = data;
-              });
-        }
-
+    function localizedModifiedTimestap() {
+      if (vm.learningGroup) {
+        return moment(vm.learningGroup.Modified).format("M/D/YYYY h:mm A");
+      } else {
+        return '';
+      }
     }
+
+    // navigate backwards in the history stack
+    function goBack() {
+      $window.history.back();
+    }
+
+    // handle save action
+    function goSave() {
+      datacontext.saveLearningGroup(vm.learningGroup)
+        .then(function () {
+          common.logger.logSuccess("Saved learning group.", null, controllerId);
+        })
+        .then(function () {
+          $location.path('/LearningGroups/');
+        });
+    }
+
+    // handle delete action
+    function goDelete() {
+      datacontext.deleteLearningGroup(vm.learningGroup)
+        .then(function () {
+          common.logger.logSuccess("Deleted learning group.", null, controllerId);
+        })
+        .then(function () {
+          $location.path('/LearningGroups/');
+        });
+    }
+
+    // load the item specified in the route
+    function getItemDetail(learningGroupId) {
+      datacontext.getLearningGroup(learningGroupId)
+        .then(function (data) {
+          vm.learningGroup = data;
+        });
+    }
+  }
 
 })();
--- ang01vle/App/services/datacontext.angular.js
+++ ang01vle/App/services/datacontext.angular.js
@@ -1,88 +1,141 @@
 ﻿(function () {
-    'use strict';
+  'use strict';
 
-    var serviceId = 'datacontext';
-    angular.module('app').factory(serviceId,
-        ['$rootScope', '$http', '$resource', '$q', 'config', 'common', 'spContext', datacontext]);
+  var serviceId = 'datacontext';
+  angular.module('app').factory(serviceId,
+    ['$rootScope', '$http', '$resource', '$q', 'config', 'common', 'spContext', datacontext]);
 
-    function datacontext($rootScope, $http, $resource, $q, config, common, spContext) {
+  function datacontext($rootScope, $http, $resource, $q, config, common, spContext) {
 
-        init()
+    init()
 
-        // service public signature
-        return {
-            // learning group members:
-            getLearningGroupsPartials: getLearningGroupsPartials,
-            getLearningGroup: getLearningGroup
+    // service public signature
+    return {
+      // learning group members:
+      getLearningGroupsPartials: getLearningGroupsPartials,
+      getLearningGroup: getLearningGroup,
+      saveLearningGroup: saveLearningGroup,
+      deleteLearningGroup: deleteLearningGroup
 
-        }
-
-        function init() {
-            common.logger.log("service loaded", null, serviceId);
-            common.logger.log("the value of $ is: " + $q, null, serviceId);
-        }
-
-        // get the Learning Group angular resource reference
-        function getLgResource(currentItem) {
-            if (+currentItem.Id) {
-                return $resource('_api/web/lists/getbytitle(\'Навчальні групи\')/items(:itemId)',
-                { itemId: currentItem.Id },
-                {
-                    get: {
-                        method: 'GET',
-                        params: {
-                            '$select': 'Id,Title,OData__Comments,Created,Modified'
-                        },
-                        headers: {
-                            'Accept': 'application/json;odata=verbose;'
-                        }
-                    }
-                });
-            }
-        }
-
-        function getLearningGroupsPartials() {
-            var deferred = $q.defer();
-
-            $http({
-                method: 'GET',
-                url: '_api/web/lists/getbytitle(\'Навчальні групи\')/items?$select=Id,Title,OData__Comments&$orderby=Title',
-            })
-            .success(function (data) {
-                common.logger.log('retrieved LGroup partials via ngHttp', data, serviceId);
-                deferred.resolve(data.d.results);
-            })
-            .error(function (error) {
-                var message = 'data context ngHttp error: ' + error.message;
-                common.logger.logError(message, error, serviceId);
-                deferred.reject(error);
-            });
-
-            return deferred.promise;
-        }
-
-        // gets a specific learning group
-        function getLearningGroup(id) {
-            var lg = new vle.models.learningGroup();
-            lg.Id = id;
-
-            // get resource
-            var resource = getLgResource(lg);
-            common.logger.log("resource is undefined?", getLgResource(lg), serviceId);
-
-            var deferred = $q.defer();
-            resource.get({}, function (data) {
-                // success callback
-                deferred.resolve(data.d);
-                common.logger.log("retrieved learning group", data, serviceId);
-            }, function (error) {
-                // failure callback
-                deferred.reject(error);
-                common.logger.logError("retrieve learning group", error, serviceId);
-            });
-
-            return deferred.promise;
-
-        }
     }
+
+    function init() {
+      common.logger.log("service loaded", null, serviceId);
+    }
+
+    // get the Learning Group angular resource reference
+    function getLgResource(currentItem) {
+      if (+currentItem.Id) {
+        return $resource('_api/web/lists/getbytitle(\'Навчальні групи\')/items(:itemId)',
+        { itemId: currentItem.Id },
+        {
+          get: {
+            method: 'GET',
+            params: {
+              '$select': 'Id,Title,OData__Comments,Created,Modified'
+            },
+            headers: {
+              'Accept': 'application/json;odata=verbose;'
+            }
+          },
+          post: {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json;odata=verbose;',
+              'Content-Type': 'application/json;odata=verbose',
+              'X-RequestDigest': spContext.securityValidation,
+              'X-HTTP-Method': 'MERGE',
+              'If-Match': currentItem.__metadata.etag
+            }
+          },
+          delete: {
+            method: 'DELETE',
+            headers: {
+              'Accept': 'application/json;odata=verbose;',
+              'Content-Type': 'application/json;odata=verbose',
+              'X-RequestDigest': spContext.securityValidation,
+              'If-Match': '*'
+            }
+          }
+        });
+      }
+    }
+
+    function getLearningGroupsPartials() {
+      var deferred = $q.defer();
+
+      $http({
+        method: 'GET',
+        url: '_api/web/lists/getbytitle(\'Навчальні групи\')/items?$select=Id,Title,OData__Comments&$orderby=Title',
+      })
+      .success(function (data) {
+        common.logger.log('retrieved LGroup partials via ngHttp', data, serviceId);
+        deferred.resolve(data.d.results);
+      })
+      .error(function (error) {
+        var message = 'data context ngHttp error: ' + error.message;
+        common.logger.logError(message, error, serviceId);
+        deferred.reject(error);
+      });
+
+      return deferred.promise;
+    }
+
+      // gets a specific learning group
+    function getLearningGroup(id) {
+      var lg = new vle.models.learningGroup();
+      lg.Id = id;
+
+      // get resource
+      var resource = getLgResource(lg);
+
+      var deferred = $q.defer();
+      resource.get({}, function (data) {
+        // success callback
+        deferred.resolve(data.d);
+        common.logger.log("retrieved learning group", data, serviceId);
+      }, function (error) {
+        // failure callback
+        deferred.reject(error);
+        common.logger.logError("retrieve learning group", error, serviceId);
+      });
+
+      return deferred.promise;
+    }
+
+    function saveLearningGroup(learningGroup) {
+      // get resource
+      var resource = getLgResource(learningGroup);
+
+      var deferred = $q.defer();
+
+      resource.post(learningGroup, function (data) {
+        deferred.resolve(data);
+        common.logger.log("save learning group", data, serviceId);
+      }, function (error) {
+        deferred.reject(error);
+        common.logger.logError("save learning group", error, serviceId);
+      });
+
+      return deferred.promise;
+    }
+
+    function deleteLearningGroup(learningGroup) {
+      // get resource
+      var resource = getLgResource(learningGroup);
+
+      var deferred = $q.defer();
+
+      // use angular $resource to delete the item
+      resource.delete(learningGroup, function (data) {
+        deferred.resolve(data);
+        common.logger.log("delete learning group", data, serviceId);
+      }, function (error) {
+        deferred.reject(error);
+        common.logger.logError("delete learning group", error, serviceId);
+      });
+
+      return deferred.promise;
+    }
+  }
 })();
